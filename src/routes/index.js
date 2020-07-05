485a01b5394638c3e088afb00713b048b86819b6
tarosun <sunxiaomingyes@gmail.com>
2020-07-05 17:58:08 +0800

    feat: 路由测试

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,4 +16,22 @@ router.get('/json', async (ctx, next) => {
   }
 })
 
+// 设置动态参数
+router.get('/profile/:userName', async (ctx, next) => {
+  const {userName} = ctx.params
+  ctx.body = {
+    title: 'this is profile page',
+    userName
+  }
+})
+
+router.get('/loadMore/:userName/:pageIndex', async (ctx, next) => {
+  const {userName, pageIndex} = ctx.params
+  ctx.body = {
+    title: 'this is loadMore page',
+    userName,
+    pageIndex
+  }
+})
+
 module.exports = router

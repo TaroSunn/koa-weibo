b2dd4da69c21b2085462889d6a0dc0558aa22211
tarosun <sunxiaomingyes@gmail.com>
2020-07-06 22:48:08 +0800

    feat: 连接池配置

--- src/model/seq.js
+++ src/model/seq.js
@@ -1,10 +1,19 @@
 const Sequelize = require('sequelize')
 
-// 连接数据库配置
-const seq = new Sequelize('koa_weibo_db', 'root', 'rootroot', {
+const conf = {
   host: 'localhost',
   dialect: 'mysql'
-})
+}
+
+// conf.poof = {
+//   max: 5, // 连接池最大数量
+//   min: 0, // 最小数量
+//   idle: 10000, // 10s内不使用释法
+// }
+
+// 连接数据库配置
+const seq = new Sequelize('koa_weibo_db', 'root', 'rootroot', conf)
+
 
 // 测试连接
 // seq.authenticate().then(() => {
